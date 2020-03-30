8da0938489ac592dc22ce5f127e10ed04f1d7a63
code4god <gearendse@gmail.com>
2020-03-30 19:32:41 +0200

    Data to populate tables
    Data to populate Postal Code and CalculationTypes Tables

---
+++ data.sql
@@ -0,0 +1,24 @@
+INSERT INTO PostalCodes
+VALUES('7441', SYSDATETIME())
+
+INSERT INTO PostalCodes
+VALUES('A100', SYSDATETIME())
+
+INSERT INTO PostalCodes
+VALUES('7000', SYSDATETIME())
+
+INSERT INTO PostalCodes
+VALUES('1000', SYSDATETIME())
+
+
+INSERT INTO CalculationTypes
+VALUES(1,'Progressive', SYSDATETIME())
+
+INSERT INTO CalculationTypes
+VALUES(2,'Flat Value', SYSDATETIME())
+
+INSERT INTO CalculationTypes
+VALUES(3,'Flat rate', SYSDATETIME())
+
+INSERT INTO CalculationTypes
+VALUES(4,'Progressive', SYSDATETIME())
